5e094acf173cca85250015ee56e20e82d50b83a2
dyoma-bat <78908041+dyoma-bat@users.noreply.github.com>
2021-06-07 00:12:19 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,13 +1,87 @@
 // Copyright 2021 NNTU-CS
+//Данилов Евгений
+//20-ИСТ-3
 
-int countPairs1(int *arr, int len, int value) {
-  return 0;
+int countPairs1(int *arr, int len, int value) 
+{
+  
+  int n = 0;
+  
+  for (int i = 0; i < len-1; i++) 
+  {    
+    for (int j = i + 1; j < len; j++) 
+      {
+        if (arr[i] + arr[j] == value)
+        n++;
+      }
+  }
+  
+  return n;
+  
 }
 
-int countPairs2(int *arr, int len, int value) {
-  return 0;
+int countPairs2(int *arr, int len, int value) 
+{
+  unsigned int n = 0, 
+               i = 0, 
+               k = 0, 
+               r = len - 1;
+  
+  while (k < r) 
+  {
+   i = 1;
+   while (true) 
+   {
+    if (arr[r] > value || arr[k] + arr[r] > value) 
+    {
+                r--;
+            } else {
+                break;
+            }
+        }
+        while (k+ i <= r) 
+        {
+            if (arr[k] + arr[k + i] == value) n++;
+            i++;
+        }
+        k++;
+    }
+    return n;
 }
 
-int countPairs3(int *arr, int len, int value) {
-  return 0;
+int countPairs3(int *arr, int len, int value) 
+{
+  int n = 0;
+
+  for (int i = 0; i < len - 1; i++) 
+  {
+
+  int left = i, right = len;
+    while (left < right-1) 
+    {
+    int middle = (left + right) / 2;
+      if (arr[i] + arr[middle] == value) 
+      {
+      n++;
+      int j = middle+1;
+        while (arr[i] + arr[j] == value && j <right) 
+        {        
+        n++;
+        j++;
+        }
+      j = middle - 1;
+          while (arr[i] + arr[j] == value && j > left) 
+          {
+          n++;
+          j--;
+          }
+       break;
+       }
+          if (arr[i] + arr[middle] > value)
+          right = middle;
+          else
+          left = middle;
+     }
+  }
+return n;
 }
